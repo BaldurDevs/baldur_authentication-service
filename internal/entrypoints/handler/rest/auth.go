e641ca0d572f0988a6c40d8395d172962d042905
nachoberaza <nachoberaza@gmail.com>
2023-01-15 18:30:48 -0300

    Correction with a handler error

--- internal/entrypoints/handler/rest/auth.go
+++ internal/entrypoints/handler/rest/auth.go
@@ -38,13 +38,9 @@ func (handler *AuthHandler) authenticate(c *gin.Context) {
 	}
 
 	result, err := handler.authenticateUser.Execute(c, authUserRequest)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, nil)
-		return
-	}
-
-	if !result {
+	if err != nil || !result {
 		c.JSON(http.StatusUnauthorized, map[string]any{"message": "wrong email or password"})
+		return
 	}
 
 	c.JSON(http.StatusNoContent, nil)
